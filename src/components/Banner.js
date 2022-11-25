5cb9953a21e9af5d4deb014ccb2e280b449d8154
LolaGuigand <73544896+LolaGuigand@users.noreply.github.com>
2022-11-25 09:56:24 +0100

    1 binaries uploaded

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import '../styles/Banner.css';
+
 function Banner() {
   const title = '🌱 La maison jungle 🌿';
-  return <h1>{title.toUpperCase()}</h1>;
+  return <h1 className="lmj-title">{title.toUpperCase()}</h1>;
 }
 
 export default Banner;
